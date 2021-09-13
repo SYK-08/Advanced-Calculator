49abd80f43a8296263f1649ad454f48a4059389a
Shreyash Kashyap <76607993+SYK-08@users.noreply.github.com>
2021-09-13 20:57:33 +0530

    Worked on efficiency and cleanliness

--- AdvancedCalculator.py
+++ AdvancedCalculator.py
@@ -1,38 +1,35 @@
-q = input("Should I start the program?(y/n)")
-if q == 'n':
-    print("Oh okay.")
-elif q == 'y':
-    print("Select operation:\n","1.Addition(+)\n","2.Subtraction(-)\n","3.Multiplication(*)\n","4.Division(/)\n","5.Exponentiation(**)\n","6.Modulo(%)\n","7.Floored quotient or Integer division(//)\n")
-
 def Input():
+    print("Select operation:\n","1.Addition(+)\n","2.Subtraction(-)\n","3.Multiplication(*)\n","4.Division(/)\n","5.Exponentiation(**)\n","6.Modulo(%)\n","7.Floored quotient or Integer division(//)\n")
+    
     op = input("Operation-->")
-    num1 = input("First Number:")
-    num2 = input("Second Number:")
-    answer = num1 + op + num2
+    num1 = int(input("First Number:"))
+    num2 = int(input("Second Number:"))
+    answer = str(num1) + op + str(num2)
     print("Answer:")
 
     if op == "+":
-        print(int(num1) + int(num2))
+        print(num1 + num2)
     elif op == "-":
-        print(int(num1) - int(num2))
+        print(num1 - num2)
     elif op == "*":
-        print(int(num1) * int(num2))
+        print(num1 * num2)
     elif op == "/":
-        print(int(num1) / int(num2))
+        print(num1 / num2)
     elif op == "**":
-        print(int(num1) ** int(num2))
+        print(num1 ** num2)
     elif op == "%":
-        print(int(num1) % int(num2))
+        print(num1 % num2)
     elif op == "//":
-        print(int(num1) // int(num2))
+        print(num1 // num2)
     else:
         print("Invalid Format.")     
 Input()
 
-while True:
-    q2 = input("Do you want to calculate more?(y/n)")
-    if q2 == 'n':
-        print("Okay, bye bye.")
-        break
-    elif q2 == 'y':
-       Input()
+def again():
+    while True:
+        q1 = input("Do you want to calculate more?(y/n)").lower()
+        if q1 == 'n':
+            exit()
+        elif q1 == 'y':
+            Input()
+again()
